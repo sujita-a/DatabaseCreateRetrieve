6d82a4c575c498e7ca773149913007e8879663aa
Sujita <sujita.goit@gmail.com>
2021-02-02 08:02:55 +0545

    Happy Coding

---
+++ app/src/main/java/edu/tu/database/DbHandler.java
@@ -0,0 +1,68 @@
+package edu.tu.database;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class DbHandler extends SQLiteOpenHelper {
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME = "usersdb";
+    private static final String TABLE_Users = "userdetails";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_LOC = "location";
+    private static final String KEY_DESG = "designation";
+    public DbHandler(Context context){
+        super(context,DB_NAME, null, DB_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_Users + "("
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_NAME + " TEXT,"
+                + KEY_LOC + " TEXT,"
+                + KEY_DESG + " TEXT"+ ")";
+        db.execSQL(CREATE_TABLE);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        // Drop older table if exist
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_Users);
+        // Create tables again
+        onCreate(db);
+    }
+    // **** CRUD (Create, Read, Update, Delete) Operations ***** //
+
+    // Adding new User Details
+    void insertUserDetails(String name, String location, String designation){
+        //Get the Data Repository in write mode
+        SQLiteDatabase db = this.getWritableDatabase();
+        //Create a new map of values, where column names are the keys
+        ContentValues cValues = new ContentValues();
+        cValues.put(KEY_NAME, name);
+        cValues.put(KEY_LOC, location);
+        cValues.put(KEY_DESG, designation);
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(TABLE_Users,null, cValues);
+        db.close();
+    }
+    // Get User Details
+    public ArrayList<HashMap<String, String>> GetUsers(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ArrayList<HashMap<String, String>> userList = new ArrayList<>();
+        String query = "SELECT name, location, designation FROM "+ TABLE_Users;
+        Cursor cursor = db.rawQuery(query,null);
+        while (cursor.moveToNext()){
+            HashMap<String,String> user = new HashMap<>();
+            user.put("name",cursor.getString(cursor.getColumnIndex(KEY_NAME)));
+            user.put("designation",cursor.getString(cursor.getColumnIndex(KEY_DESG)));
+            user.put("location",cursor.getString(cursor.getColumnIndex(KEY_LOC)));
+            userList.add(user);
+        }
+        return  userList;
+    }
+
+}
